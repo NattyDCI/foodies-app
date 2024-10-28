ba77a65742c19164202c6731affcb3a0b6519ac8
NathaliaPadron <nbavutti@gmail.com>
2024-10-28 12:52:00 +0100

    fetching data from a sql-lite database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,18 @@
+import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
-import Link from "next/link"
+import Link from "next/link";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
+  
   return (
     <>
         <header className={classes.header}>
             <h1>Delicious meals, created <span className={classes.highlight}>by you</span> </h1>
             <p>Choose your favourite recipe and cook it yourself, it is easy and fun.</p>
+
             <p className={classes.cta}>
                 <Link href="/meals/share">
                     Share Your Favourite Recipe
@@ -15,8 +21,8 @@ export default function MealsPage() {
             </p>
         </header>
         <main className={classes.main}>
-            
-
+          {/* we have no meals yet, so we add an empty array */}
+            <MealsGrid meals={meals}/>
         </main>
     </>
   )
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,11 +1,16 @@
-import classes from "./meals-grid.module.css";  
+import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
 
-export default function MealsGrid({meals}) {
+
+export default function MealsGrid({ meals }) {
   return (
-    <ul>
-        {meals.map(meal => <li key={meal.id}>
-            
-        </li> )}
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          {/* i am using the spread operator to pass all the properties from the meal object that I am receiving, since the meal array will eventually have all those properties  */}
+          <MealItem {...meal}/>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
