52f159e15d03e90fa2da4b6ca4613862d59326bb
NathaliaPadron <nbavutti@gmail.com>
2024-10-30 12:06:15 +0100

    changes

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,19 @@
+"use client";
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
+import { useActionState } from "react";
 
 export default function ShareMealPage() {
+  // this hook is responsible for managing state forms that use server actions
+  //and this hooks takes in two arguments, the first is the server action that gets
+  // triggered when the form is submitted. The second argument is the initial state of this component  
+  // normally takes the shape of the value that is returned from that action and that is an object
+  
+  const [ state, formAction ] = useActionState(shareMeal, { message: null});
+
   
   return (
     <>
@@ -14,7 +24,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,7 +52,8 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-            <ImagePicker label="Your Image" name="image" />         
+            <ImagePicker label="Your Image" name="image" />  
+            {state.message && <p>{state.message}</p> }       
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
